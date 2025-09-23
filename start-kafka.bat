2b1552b94b6756123af2f1eebc4e861fbcad6b5c
Kafka Student <kafka.student@example.com>
2025-09-23 10:38:22 +0200

    Restore complete exercice6 content

--- start-kafka.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Script pour demarrer Kafka Server
-echo Demarrage de Kafka Server...
-cd /d "%~dp0kafka_2.13-3.9.1"
-bin\windows\kafka-server-start.bat config\server.properties
-pause
--- start-zookeeper.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Script pour demarrer ZooKeeper (requis pour Kafka)
-echo Demarrage de ZooKeeper...
-cd /d "%~dp0kafka_2.13-3.9.1"
-bin\windows\zookeeper-server-start.bat config\zookeeper.properties
-pause
